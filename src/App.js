c1b23469b2ff00bf7473696267b19e49d092255e
Manibala <manibalak@gmail.com>
2023-11-27 14:14:26 -0500

    third message

--- src/App.js
+++ src/App.js
@@ -1,52 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Calculation from './components/Calculation';
-import Result from './components/Result';
-
-function App() {
-  const [enteredNumbers, setEnteredNumbers] = useState({ first: 0, second: 0 });
-  const [chosenOperation, setChosenOperation] = useState('add'); // valid values: 'add', 'subtract', 'multiply', 'divide'
-
-  function changeFirstNumberHandler(event) {
-    setEnteredNumbers((prevNumbers) => ({
-      first: +event.target.value,
-      second: prevNumbers.second,
-    }));
-  }
-
-  function changeSecondNumberHandler(event) {
-    setEnteredNumbers((prevNumbers) => ({
-      first: prevNumbers.first,
-      second: +event.target.value,
-    }));
-  }
-
-  function updateOperationHandler(event) {
-    setChosenOperation(event.target.value);
-  }
-
-  let result;
-
-  if (chosenOperation === 'add') {
-    result = enteredNumbers.first + enteredNumbers.second;
-  } else if (chosenOperation === 'subtract') {
-    result = enteredNumbers.first - enteredNumbers.second;
-  } else if (chosenOperation === 'multiply') {
-    result = enteredNumbers.first * enteredNumbers.second;
-  } else {
-    result = enteredNumbers.first / enteredNumbers.second;
-  }
-
-  return (
-    <>
-      <Calculation
-        onFirstNumberChanged={changeFirstNumberHandler}
-        onSecondNumberChanged={changeSecondNumberHandler}
-        onOperationChanged={updateOperationHandler}
-      />
-      <Result calculationResult={result} />
-    </>
+function App(){
+ 
+    const [enteredEmail, setEnteredEmail]= useState();
+    const [inputIsInvalid, setInputIsInvalid] = useState(false);
+    function emailChangeHandler(event){
+      setEnteredEmail(event.target.value);
+    }
+    function submitFormHandler(event){
+      event.preventDefault();
+      const emailIsValid = enteredEmail.includes('@');
+      setInputIsInvalid(!emailIsValid);
+    }
+    return (
+      <section>
+    <form onSubmit={submitFormHandler}>
+      <label htmlFor="email">Your email</label>
+      <input type="text" id="email" onChange={emailChangeHandler}/>
+      <button>Submit</button>
+    </form>
+    {inputIsInvalid && <p>Invalid email address entered!</p>}
+    </section>
   );
 }
-
 export default App;
